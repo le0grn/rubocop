44bef76f281bc4409ad69261d70e242be9ab3003
le0grn <leogrn08@gmail.com>
2026-02-07 12:16:08 -0300

    refactor: remove irrelevant boilerplate bridgetown files, add rubocop.org to config/initializers.rb and set metadata on src/_data/site_metadata.yml

--- config/initializers.rb
+++ config/initializers.rb
@@ -16,7 +16,7 @@
 
 Bridgetown.configure do |config|
   # The base hostname & protocol for your site, e.g. https://example.com
-  url ""
+  url "https://rubocop.org"
 
   # Available options are `erb` (default), `serbea`, or `liquid`
   template_engine "erb"
