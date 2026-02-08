08fe7616f3227cbd9738c4a22ef036f679688953
le0grn <leogrn08@gmail.com>
2026-02-08 18:36:47 -0300

    chore: add temporary env var to be able to test on github pages with assets

--- config/initializers.rb
+++ config/initializers.rb
@@ -46,7 +46,7 @@ Bridgetown.configure do |config|
   # If you're using esbuild for frontend assets, edit `esbuild.config.js` to
   # update `publicPath`.
   #
-  # base_path "/"
+  base_path ENV.fetch("BASE_PATH", "/")
 
   # You can also modify options on this configuration object directly, like so:
   #
--- esbuild.config.js
+++ esbuild.config.js
@@ -1,10 +1,13 @@
 import build from "./config/esbuild.defaults.js"
 
+const basePath = process.env.BASE_PATH || ""
+
 /**
  * @typedef { import("esbuild").BuildOptions } BuildOptions
  * @type {BuildOptions}
  */
 const esbuildOptions = {
+  publicPath: `${basePath}/_bridgetown/static`,
   globOptions: {
     excludeFilter: /\.(dsd|lit)\.css$/
   }
